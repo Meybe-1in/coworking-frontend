e263d4f32e6a91efee459d5dc633a1f2726b7f8b
Meybelin <perdomomeybel@gmail.com>
2025-10-23 17:23:14 -0600

    feat: Configurar enrutamiento principal con rutas protegidas Define la estructura de rutas. Incluye rutas públicas (/Login, /register, /) y rutas protegidas (como /rooms y /reservations/new)

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import SignUp from "./pages/SignUp/SignUp";
+import RoomsList from "./pages/RoomsList";
+import CreateReservation from "./pages/CreateReservation";
+import LoginPage from "./pages/Login/LoginPage";
+import Home from "./pages/HomePage/HomePage"
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+function LayoutWrapper() {
+  const location = useLocation();
+
+  // rutas donde NO se mostrará el layout general (navbar, contenedor, etc.)
+  const hideLayoutRoutes = ["/Login", "/register"];
+  const shouldHideLayout = hideLayoutRoutes.includes(location.pathname);
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home maneja su propio Navbar y Footer */}
+          <Route path="/register" element={<SignUp />} />
+          <Route
+            path="/rooms"
+            element={
+              <ProtectedRoute>
+                <RoomsList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/reservations/new"
+            element={
+              <ProtectedRoute>
+                <CreateReservation />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/Login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <LayoutWrapper />
+    </Router>
+  );
+}
