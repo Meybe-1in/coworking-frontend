11d45182572d668e6917d615fecba9cc133042ba
Meybelin <perdomomeybel@gmail.com>
2025-10-23 18:01:58 -0600

    feat(page): Crear página principal
    Ensambla la página de inicio de la aplicación, integrando todos los componentes de la landing page: Navbar, HeroSection, About, Features, Contact y Footer.

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "../../components/NavBar/Navbar";
+import "./HomePage.css";
+import HeroSection from "../../components/HeroSection/HeroSection";
+import Features from "../../components/Features/Features";
+import About from "../../components/About/About";
+import Contact from "../../components/Contact/Contact";
+import Footer from "../../components/Footer/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="homepage-container">
+        <HeroSection />
+        <About />
+        <Features />
+        <Contact />
+      </div>
+      <Footer />
+    </>
+
+  );
+};
+
+export default HomePage;
