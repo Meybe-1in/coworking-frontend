c6e27268008930a03fbe830d46ca244711a48283
Meybelin <perdomomeybel@gmail.com>
2026-01-07 21:14:42 -0600

    Refactor: mejora visual

--- src/components/SignupForm/SignUpForm.jsx
+++ src/components/SignupForm/SignUpForm.jsx
@@ -91,7 +91,11 @@ function SignUpForm() {
 
       </div>
 
-      <Button text="Sign up" type="submit" variant="primary" />
+      <Button
+        text="Sign up"
+        type="submit"
+        variant="primary"
+      />
     </form>
   );
 }
