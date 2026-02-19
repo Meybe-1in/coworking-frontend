cba01d972c5374fd9d4cebd7fb12962e4b978e1b
Meybelin <perdomomeybel@gmail.com>
2026-02-19 15:45:02 -0600

    feat(api): agregar llamada ala APIa getCalendar para el calendario

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -102,6 +102,15 @@ export const getAvailableRooms = async (filters) => {
   }
 }
 
+//crear calendario
+export const getCalendar = async (from, to) => {
+  
+    const res = await API.get(
+     "/api/reservations/calendar", 
+      { params: { from, to } }
+    );
+    return res.data;
+}
 
 export default API;
 
