3fec2725eed0e4e6221a3b6d0f3f5676eea43b24
Meybelin <perdomomeybel@gmail.com>
2026-02-25 21:47:39 -0600

    fix(calendar): Usar events={loadEvents}  en lugar de useState/events

--- src/components/Calendar/ReservationCalendar.jsx
+++ src/components/Calendar/ReservationCalendar.jsx
@@ -1,24 +1,26 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import { useEffect, useState } from "react";
 import { getCalendar } from "../../api/axiosConfig";
 
+
 export default function ReservationCalendar() {
-  const [events, setEvents] = useState([]);
+  const loadEvents = async (info, successCallback, failureCallback) => {
+    try {
+      const data = await getCalendar(info.startStr, info.endStr);
 
-  const loadEvents = async (info) => {
-    const data = await getCalendar(info.startStr, info.endStr);
-
-    setEvents(
-      data.map(e => ({
+      const formatted = data.map(e => ({
         title: e.title,
         start: e.start,
         end: e.end,
         display: "background",
         backgroundColor: "#ef4444"
-      }))
-    );
+      }));
+
+      successCallback(formatted);
+    } catch (error) {
+      failureCallback(error);
+    }
   };
 
   return (
@@ -26,14 +28,13 @@ export default function ReservationCalendar() {
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin]}
         initialView="timeGridWeek"
-        events={events}
-        slotMinTime="07:00:00"   // empieza a las 7 AM
-        slotMaxTime="21:00:00"   // termina a las 8 PM
+        events={loadEvents} 
+        slotMinTime="07:00:00"
+        slotMaxTime="21:00:00"
         allDaySlot={false}
         selectable={false}
         editable={false}
         height="auto"
-        eventsSet={loadEvents}
       />
     </div>
   );
