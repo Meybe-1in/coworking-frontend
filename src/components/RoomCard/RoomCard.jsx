f3d7972d222c81fe872cbf4ff17f302b5a7bb5f5
Meybelin <perdomomeybel@gmail.com>
2026-03-15 19:27:13 -0600

    feat: RoomCard ahora soporta modo preview

--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -1,21 +1,58 @@
 import { useNavigate } from "react-router-dom";
-export default function RoomCard({ room, filters }) {
+
+export default function RoomCard({ room, filters, preview = false }) {
   const isAvailable = room.available;
   const navigate = useNavigate();
 
   const handleReserve = () => {
     if (!filters) return;
-      navigate("/checkout", { state: { room, filters } });
+    navigate("/checkout", { state: { room, filters } });
   };
+
+  const goToRoom = () => {
+    navigate(`/rooms/${room.id}`);
+  };
+
+  if (preview) {
+    return (
+      <div
+        onClick={goToRoom}
+        className="cursor-pointer bg-white rounded-xl shadow hover:shadow-lg transition overflow-hidden"
+      >
+        <img
+          src={
+            room.imageUrl
+              ? `${import.meta.env.VITE_API_URL}${room.imageUrl}`
+              : "https://via.placeholder.com/400x300?text=No+Image"
+          }
+          alt={room.name}
+          className="w-full h-40 object-cover"
+        />
+
+        <div className="p-4 flex flex-col gap-1">
+
+          <h3 className="font-semibold text-lg">{room.name}</h3>
+
+          <p className="text-gray-600 text-sm">
+            {room.capacity} personas
+          </p>
+
+          <p className="font-bold text-blue-800">
+            ${room.price}/h
+          </p>
+
+        </div>
+      </div>
+    );
+  }
 
   return (
     <article className="bg-white rounded-2xl shadow-xl p-6">
       {/* Header */}
       <div className="flex justify-between">
         <span
-          className={`px-6 py-2 rounded-br-xl text-white font-semibold ${
-            isAvailable ? "bg-sky-500" : "bg-red-500"
-          }`}
+          className={`px-6 py-2 rounded-br-xl text-white font-semibold ${isAvailable ? "bg-sky-500" : "bg-red-500"
+            }`}
         >
           {isAvailable ? "Disponible" : "No disponible"}
         </span>
@@ -56,10 +93,9 @@ export default function RoomCard({ room, filters }) {
             onClick={handleReserve}
             className={`
               mt-4 px-6 py-3 rounded-full font-medium transition
-              ${
-                isAvailable
-                  ? "bg-sky-500 text-white hover:bg-blue-800 cursor-pointer"
-                  : "bg-gray-400 text-gray-700 cursor-not-allowed opacity-70"
+              ${isAvailable
+                ? "bg-sky-500 text-white hover:bg-blue-800 cursor-pointer"
+                : "bg-gray-400 text-gray-700 cursor-not-allowed opacity-70"
               }
             `}
           >
