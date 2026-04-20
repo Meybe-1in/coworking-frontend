426ed240687f174ef58079e78af22a90eb0a904f
Meybelin <perdomomeybel@gmail.com>
2026-04-20 14:24:11 -0600

    feat: permitir rutas publicas

--- src/components/Auth/AuthWatcher.jsx
+++ src/components/Auth/AuthWatcher.jsx
@@ -4,12 +4,24 @@ import { useNavigate } from "react-router-dom";
 export default function AuthWatcher() {
   const navigate = useNavigate();
 
+  const publicRoutes = [
+    "/",
+    "/login",
+    "/register",
+    "/terms",
+    "/privacy",
+    "/forgot-password",
+    "/reset-password"
+  ];
+
   useEffect(() => {
     const token =
       localStorage.getItem("token") ||
       sessionStorage.getItem("token");
 
-    if (!token) {
+
+
+    if (!token && !publicRoutes.includes(location.pathname)) {
       navigate("/login");
     }
   }, [navigate]);
