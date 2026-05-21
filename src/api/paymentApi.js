8091794e42d63a8ebd411d80b05b4d7404ce8b19
Meybelin <perdomomeybel@gmail.com>
2026-05-21 15:01:07 -0600

    feat:Implementar pantalla de historial de pagos del usuario

--- src/api/paymentApi.js
+++ src/api/paymentApi.js
@@ -4,3 +4,9 @@ export const createPaymentIntent = async (reservationId) => {
   const res = await API.post(`/payments/intent/${reservationId}`);
   return res.data;
 };
+
+
+export const getPayments = async ( ) => {
+  const res = await API.get("/payments/my-history");
+  return res.data;
+}
