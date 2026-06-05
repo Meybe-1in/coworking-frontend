bfadc7de6c57f72c89dc35114ceef1d2a0f0d8b7
Meybelin <perdomomeybel@gmail.com>
2026-06-05 16:10:33 -0600

    feat: Agregar funciones para descarga de reportes CSV

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -19,3 +19,17 @@ export const getAllPayments = async () =>{
     const response = await API.get("/admin/payments");
     return response.data;
 }
+
+export const exportReservationsCSV = async () =>{
+    const response = await API.get("/admin/reports/reservations", {
+        responseType: "blob",
+    });
+    return response.data;
+}
+
+export const exportPaymentsCSV = async () =>{
+    const response = await API.get("/admin/reports/payments", {
+        responseType: "blob",
+    });
+    return response.data;
+}
