148dffdcf777c53e2c0a9ac2ab7585bc22f499c0
Meybelin <perdomomeybel@gmail.com>
2026-09-07 22:02:53 -0600

    feat: agregar endpoint para exportar csv, y agregar paginacion

--- src/api/auditApi.js
+++ src/api/auditApi.js
@@ -1,12 +1,39 @@
 import API from "./axiosConfig";
 
-export const getAuditLogs = async (page = 0, size = 10) => {
+export const getAuditLogs = async (
+  page = 0,
+  size = 10,
+  filters = {}
+) => {
   const response = await API.get("/admin/audit-logs", {
     params: {
       page,
       size,
+      ...(filters.adminName && {
+        adminName: filters.adminName,
+      }),
+      ...(filters.startDate && {
+        startDate: filters.startDate,
+      }),
+      ...(filters.endDate && {
+        endDate: filters.endDate,
+      }),
     },
   });
+
+  return response.data;
+};
+
+export const exportAuditLogsCSV = async (
+  filters = {}
+) => {
+  const response = await API.post(
+    "/admin/audit-logs/export/csv",
+    filters,
+    {
+      responseType: "blob",
+    }
+  );
 
   return response.data;
 };
